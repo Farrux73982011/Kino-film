3d81e3bf67a5723f6f89663b928d698bbf5435be
Developer <112705739+Farrux73982011@users.noreply.github.com>
2023-09-27 18:07:37 +0500

    site ready

--- js/script.js
+++ js/script.js
@@ -1,109 +1,114 @@
-
-import { movies } from "./db.js";
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
-
-// const movieDB = {
-//     movies: [
-//         "Логан",
-//         "Лига справедливости",
-//         "Ла-ла лэнд",
-//         "Одержимость",
-//         "Скотт Пилигрим против..."
-//     ]
-// };
-
-let genres = movies.map((item) => item.Genre);
-genres = ["All", ...new Set(genres)];
-
-let adds = document.querySelector('.promo__adv')
-adds.innerHTML = ' '
-
-let logo = document.querySelector('.promo__bg')
-
-logo.style.backgroundImage = 'url(../img/bg.jpg)';
-
+import {
+    movies
+} from "./db.js";
+let genres =  movies.map((item) => item.Genre)
+genres = [...new Set(genres)]
+let ul_class = document.querySelector('.promo__menu-list ul')
 let promo__interactive = document.querySelector('.promo__interactive-list')
-
 let bac = document.querySelector(".promo__bg");
 let gen = document.querySelector(".promo__genre");
 let title = document.querySelector(".promo__title");
 let text = document.querySelector(".promo__descr");
-let IMBd = document.querySelector(".IMBd");
-let kinopoisk = document.querySelector(".kinopoisk");
-let h1 = document.querySelector('.h1')
-let h2 = document.querySelector('.h2')
-let h3 = document.querySelector('.h3')
-let h4 = document.querySelector('.h4')
-let h5 = document.querySelector('.h5')
-let h6 = document.querySelector('.h6')
-let h7 = document.querySelector('.h7')
-let h8 = document.querySelector('.h8')
-let h9 = document.querySelector('.h9')
-let h10 = document.querySelector('.h10')
-let h11 = document.querySelector('.h11')
+let inp_search = document.querySelector('#search')
+let header__logo = document.querySelector('.header__logo img')
+// let compStyle = window.getComputedStyle(inp_search.parentElement)
+// let afterElement = compStyle.getPropertyValue(':after')
 
-let filterfordel = [...movies];
-reload(filterfordel);
+inp_search.onkeyup = (e) => { 
+    let val = inp_search.value.toLowerCase().trim()
+
+    let filtered = movies.filter((item) => {
+        let {Title} = item
+
+        if(Title.toLowerCase().includes(val)) {
+            return item
+        }
+    })
+
+    reload(filtered)
+}
+
+
+reload(movies);
+
 function reload(data) {
-    
+    promo__interactive.innerHTML = ""
+    setMovie(data[0])
 
-for(let item of movies){
-    let div = document.createElement('div')
-    let li = document.createElement('h1')
-    let dele = document.createElement('div')
+    for (let item of data) {
+        let idx = data.indexOf(item) + 1
 
-    li.classList.add('promo__interactive-item')
-    dele.classList.add('delete')
+        let li = document.createElement('li')
+        let dele = document.createElement('div')
 
-    li.innerHTML = item.Title
+        li.classList.add('promo__interactive-item')
+        dele.classList.add('delete')
 
-    promo__interactive.append(div)
-    div.append(li, dele)
+        li.innerHTML = `${idx}. ${item.Title.slice(0, 20)}`
 
-    dele.onclick = () => {
-        data = data.filter((event) => event.ID !== event.ID);
-    };
+        li.append(dele)
+        promo__interactive.append(li)
 
-    li.onclick = () => {
-        bac.style.backgroundImage = `url("${item.Poster}")`;
-        gen.innerHTML = item.Genre;
-        title.innerHTML = item.Title;
-        text.innerHTML = item.Plot;
-        IMBd.innerHTML = `IMDb: ${item.imdbRating}`;
-        kinopoisk.innerHTML = `Кинопоиск: ${item.Metascore}`;
-        h1.innerHTML = `Titel: ${item.Title}`
-        h2.innerHTML =  `Year: ${item.Year}`
-        h3.innerHTML =  `Runtime: ${item.Runtime}`
-        h4.innerHTML =  `Genre: ${item.Genre}`
-        h5.innerHTML =  `Director: ${item.Director}`
-        h6.innerHTML =  `Writer: ${item.Writer}`
-        h7.innerHTML =  `Actors: ${item.Actors}`
-        h8.innerHTML =  `Plot: ${item.Plot}`
-        h9.innerHTML =  `Language: ${item.Language}`
-        h10.innerHTML =  `Country: ${item.Country}`
-        h11.innerHTML =  `Awards: ${item.Awards}`
-    };
+        dele.onclick = () => {
+            let idx = data.indexOf(item)
+            data.splice(idx, 1)
+        };
+
+        li.onclick = () => {
+            setMovie(item)
+        };           
+    }
 }
+
+
+reload_genres(genres)
+
+function reload_genres(data) {
+    ul_class.innerHTML = ''
+
+    for(let item of data){
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+            
+        a.innerHTML = item
+        a.classList.add('promo__menu-item')
+        a.href = "#"
+
+        li.append(a)
+        ul_class.append(li)
+    }
+
 }
+
+
+function setMovie(item) {
+    bac.style.backgroundImage = `url("${item.Poster}")`;
+    gen.innerHTML = item.Genre;
+    title.innerHTML = item.Title;
+    text.innerHTML = item.Plot;
+}
+
+
 let promo__menu = document.querySelectorAll('.promo__menu-item')
-
 promo__menu.forEach((btn) => {
     btn.onclick = () => {
         promo__menu.forEach(e => e.classList.remove('promo__menu-item_active'))
         btn.classList.add('promo__menu-item_active')
+    
+        let val = btn.value
+    
+        let filttrr = movies.filter((item) => {
+   
+            if(item.Genre.includes(val)) {
+                return item
+            }
+        })
+    
+        reload(filttrr)
     }
 })
+
+
+
+
+    
